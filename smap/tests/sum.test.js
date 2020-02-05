4c5181925b472a7f3e7506a45ee7a62008a4cbba
Sarah Bunger <bunger20@up.edu>
2020-02-05 12:18:44 -0800

    Added unit test
    The test is just the sum function from hw3; a sanity check

---
+++ smap/tests/sum.test.js
@@ -0,0 +1,6 @@
+// Bunger, Liao, Marcinko, Regier, Robinson
+
+const sum = require('../public/javascripts/sum.js');
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+});
