631edbd5ba49328a57ce3d30e2d6bd1274b8a764
Ryan Regier <42758614+regier21@users.noreply.github.com>
2020-02-06 19:03:50 -0800

    Began work on data api

--- smap/routes/api.js
+++ smap/routes/api.js
@@ -1,18 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(function(req, res, next){
-  console.log("api routing");
-  next();
-});
-
-/* GET home page. */
+/* TODO: Add API documentation. */
 router.get('/', function(req, res, next) {
   next(createError(404));
 });
 
 router.get('/cats', function(req, res, next) {
   res.json(['crime_rate', 'salary', 'gdp']);
+});
+
+router.get('/data', function(req, res, next){
+  console.log(req);
+  /*console.log(req.body);
+  console.log(req.headers);
+  console.log(req.path);*/
+  res.send("Nothing to see here")
 });
 
 module.exports = router;
