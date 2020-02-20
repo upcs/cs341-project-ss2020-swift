f74b0764e2d1c127121ce298b734f521a0abcb35
Sarah Bunger <bunger20@up.edu>
2020-02-20 10:01:15 -0800

    normalizing fxn, dbms added
    Various levels of progress made on server-side POST and normalizing function, dbms added

--- smap/public/javascripts/metadata.js
+++ smap/public/javascripts/metadata.js
@@ -2,4 +2,10 @@ $("document").ready(function(){
     $(".metadata_button").click(function(e) {
         window.alert("This is the metadata about your statistic. More will be added soon.");
     });
+    //add in a data argument, grabbed the stat the user has just selected (intead of the hardcoded value)
+    $.post( "/api", {stat : "crime_rate"}, function( data, status, xhr ) {            
+        console.log("sent a POST");
+        //update the map with the information from the JavaScript Object that comes back, which is NOT in JSON notation at this point
+        
+    });
 });
--- smap/routes/api.js
+++ smap/routes/api.js
@@ -36,4 +36,39 @@ router.get('/data', function(req, res, next){
   res.json(contents);
 });
 
+//server side post -------------------------------------------------------------
+router.post('/', function(req, res, next){
+  
+  console.log("req body ", req.body);
+
+  // //testing of reading files
+  // console.log("lines from the passwords file: " )
+  // //preparing to read in a line from the passwords.txt file for use as the host/db/user/password variables
+  // const readInterface = readline.createInterface({
+  //   input: fs.createReadStream('../routes/passwords.txt'),
+  //   output: process.stdout,
+  //   console: false
+  // });
+
+  // readInterface.on('line', function(line) {
+  //   post = line;
+  //   console.log(line);
+  // });
+
+  //query for data from the selected category
+  dbms.dbquery("SELECT crime_rate FROM ??????", 
+    function(error, results){
+      //the results that come back are JSON objects for each of the applicable entries, with each column as the key
+
+      // after testing this, DO NOT commit with the password stuff intact!
+
+      //make and send back a JSON of the 3 topping types as keys and the quantity as values
+      toppingData = {"cherry" : numCherry, "plain" : numPlain, "chocolate" : numChocolate};
+      console.log(toppingData);
+      res.send(toppingData);
+    }
+  );
+
+});
+
 module.exports = router;
