11a729cda938b254ca7ae3a018c371dafbbf1e8f
robinspi <robinsph21@up.edu>
2020-02-20 11:12:09 -0800

    Updated javascripts
    Separated out all the javascript and remove unneeded functionality. This hopefully will work well and merge with Ryan's code

--- smap/public/javascripts/metadata.js
+++ smap/public/javascripts/metadata.js
@@ -1,5 +1,8 @@
 $("document").ready(function(){
-    $(".metadata_button").click(function(e) {
-        window.alert("This is the metadata about your statistic. More will be added soon.");
+    $(".statistic-slider-metadata").click(function(e) {
+        window.alert("This is the metadata about your statistic (from slider card). More will be added soon.");
+    });
+    $(".statistic-option-metadata").click(function(e) {
+        window.alert("This is the metadata about your statistic (from selector card). More will be added soon.");
     });
 });
