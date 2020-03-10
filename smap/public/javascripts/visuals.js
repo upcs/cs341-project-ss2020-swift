1a23704bef8fa8cc50f5181f07800678ca3e4ae4
robinspi <robinsph21@up.edu>
2020-03-10 10:54:08 -0700

    Finished Javascript consolidation and cleanup
    Updating the links in index.html

--- smap/public/javascripts/visuals.js
+++ smap/public/javascripts/visuals.js
@@ -123,6 +123,25 @@ $("document").ready(function () {
         // Recolor the map
         displayWeights();
     });
+
+
+
+    // Scrolling effects
+    var lastMove = 0;
+    // TODO: Fix to work when about section exists.
+    $("#arrow").click( function() {
+        $(window).scrollTop(789);
+    });
+
+    $(window).scroll( function() {
+        scroll_val = Math.floor($(this).scrollTop());
+        console.log("Scroll value: " + scroll_val);
+        if(Date.now() - lastMove > 16) {
+            $("#settings").css("left", -1*scroll_val);
+            $("#map-container").css("filter", "opacity(" + ((888 - scroll_val) / 888) + ")");
+            lastMove = Date.now();
+        }
+    });
 });
 
 //Creates an active slider from the template and adds it to the page
