18b8ed508c0f43e766172dff841247416bcb2b79
Ryan Regier <42758614+regier21@users.noreply.github.com>
2020-03-18 21:27:22 -0700

    Removed test

--- smap/tests/sum.test.js
+++
@@ -1,6 +0,0 @@
-// Bunger, Liao, Marcinko, Regier, Robinson
-
-const sum = require('../public/javascripts/sum.js');
-    test('adds 1 + 2 to equal 3', () => {
-        expect(sum(1, 2)).toBe(3);
-});
