d6ea6206658740b5b39f6481b03c872148b5b650
robinspi <robinsph21@up.edu>
2020-03-23 22:32:15 -0700

    Update effect values

--- smap/public/javascripts/visuals.js
+++ smap/public/javascripts/visuals.js
@@ -159,8 +159,8 @@ $("document").ready(function () {
         let scroll_val = Math.floor($(window).scrollTop());
         if(window.performance.now() - lastMove > 33) {
             $("#settings").css("left", -1*scroll_val);
-            $("#map-container").css("filter", "opacity(" + ((700 - scroll_val) / 400) + ")");
-            $("#map-legend-container").css("filter", "opacity(" + ((700 - scroll_val) / 400) + ")");
+            $("#map-container").css("filter", "opacity(" + ((550 - scroll_val) / 350) + ")");
+            $("#map-legend-container").css("filter", "opacity(" + ((550 - scroll_val) / 350) + ")");
             lastMove = window.performance.now();
         }
     });
