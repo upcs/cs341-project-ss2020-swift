1586a8a31421579b889e697377c6e8d122b0c1a8
robinsph21 <robinsph21@up.edu>
2020-04-14 11:22:14 -0700

    Aspect ratio changer working, added min restraints

--- smap/public/javascripts/aspect_ratio.js
+++ smap/public/javascripts/aspect_ratio.js
@@ -1,23 +1,54 @@
 'use strict';
 
-function setLayout(){
-    let aspect_ratio = $(window).width() / $(window).height();
-    if(aspect_ratio < 1) {
-        console.log(aspect_ratio);
-        $("#horizontal-layout").prop("disabled", true);
-        $("#vertical-layout").prop("disabled", false);
+const MIN_WIDTH = 500;
+const MIN_HEIGHT = 500;
+const MIN_HORIZONTAL_WIDTH = 1000;
+const CRITICAL_ASPECT_RATIO = 1.125;
+const LAYOUT_CHANGE_TIME = 125;
+
+var orientation = null;
+
+/**
+ * @param {Boolean} init this variable says if the color flash should happen
+ * @notes this function changes the stylesheet if the aspect ratio is closer to vertical, or if the
+ *      horizontal width of the window is too small to allow for good sidebar usage
+ */
+function setLayout(init){
+    if($(window).height() < MIN_HEIGHT || $(window).width() < MIN_WIDTH) {
+        console.error("Too small window size");
+        $("#error").css("display", "grid");
     } else {
-        console.log(aspect_ratio);
-        $("#vertical-layout").prop("disabled", true);
-        $("#horizontal-layout").prop("disabled", false);
+        $("#error").css("display", "none");
+        let aspect_ratio = $(window).width() / $(window).height();
+        if(aspect_ratio < CRITICAL_ASPECT_RATIO || $(window).width() < MIN_HORIZONTAL_WIDTH) {
+            if(orientation !== "vertical") {
+                orientation = "vertical";
+                if(!init) { $("body").animate({opacity: "0"}, LAYOUT_CHANGE_TIME, "swing"); }
+                window.setTimeout(function() {
+                    $("#horizontal-layout").prop("disabled", true);
+                    $("#vertical-layout").prop("disabled", false);
+                }, LAYOUT_CHANGE_TIME);
+                if(!init) { $("body").animate({opacity: "1"}, LAYOUT_CHANGE_TIME, "swing"); }
+            }
+        } else {
+            if(orientation !== "horizontal") {
+                orientation = "horizontal";
+                if(!init) { $("body").animate({opacity: "0"}, LAYOUT_CHANGE_TIME, "swing"); }
+                window.setTimeout(function() {
+                    $("#vertical-layout").prop("disabled", true);
+                    $("#horizontal-layout").prop("disabled", false);
+                }, LAYOUT_CHANGE_TIME);
+                if(!init) { $("body").animate({opacity: "1"}, LAYOUT_CHANGE_TIME, "swing"); }
+            }
+        }
     }
 }
 
 $(document).ready(function(){
-    setLayout();
+    setLayout(true);
     var resizeTimer;
     $(window).resize(function() {
         clearTimeout(resizeTimer);
-        resizeTimer = setTimeout(setLayout, 100);
+        resizeTimer = setTimeout(() => { setLayout(false) }, 100);
     });
 });
